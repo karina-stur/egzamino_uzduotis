f967e865d431252bec716d529f091111d7a1ec96
karina-stur <sturonaite.k@gmail.com>
2024-12-29 20:39:06 +0200

    Create funkcija.h

---
+++ header/funkcija.h
@@ -0,0 +1,18 @@
+#ifndef FUNKCIJA_H
+#define FUNKCIJA_H
+
+#include <string>
+#include <unordered_map>
+#include <map>
+#include <set>
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <algorithm>
+#include <cctype>
+#include <regex>
+#include <iomanip>
+#include <stdexcept>
+
+#endif // FUNKCIJA_H
