2290ec1b13f148ec02477c5c0f08eebfe2987825
karina-stur <sturonaite.k@gmail.com>
2024-12-29 20:39:30 +0200

    Update funkcija.h

--- header/funkcija.h
+++ header/funkcija.h
@@ -15,4 +15,14 @@
 #include <iomanip>
 #include <stdexcept>
 
+std::string normalizuotiZodi(const std::string& word);
+std::string tekstasIsFailo(const std::string& filename);
+std::unordered_map<std::string, int> zodziuSkaicius(const std::string& text);
+std::unordered_map<std::string, int> rastiPasikartojimus(const std::unordered_map<std::string, int>& wordCounts);
+std::unordered_map<std::string, std::set<int>> indeksuKurimas(const std::string& text);
+std::vector<std::string> rastiUrl(const std::string& text);
+void pasikartojimuFailas(const std::unordered_map<std::string, int>& repeatedWords, const std::string& filename);
+void indeksuFailas(const std::unordered_map<std::string, std::set<int>>& crossReference, const std::string& filename);
+void urlFailas(const std::vector<std::string>& urls, const std::string& filename);
+
 #endif // FUNKCIJA_H
